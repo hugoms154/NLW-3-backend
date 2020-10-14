d526d8ed2bb8bd44fc581c654c6a62514bd3d4d2
hugo miranda <hugo.miranda.souza@gmail.com>
2020-10-14 14:40:23 -0300

    feat: application routes

---
+++ src/routes/orphanages.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import OrphanagesController from '../controllers/OrphanagesController';
+
+const orphanageRouter = Router();
+
+orphanageRouter.get('/', OrphanagesController.index);
+orphanageRouter.get('/:id', OrphanagesController.show);
+orphanageRouter.post('/', OrphanagesController.create);
+
+export default orphanageRouter;
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,12 @@ import express, { json } from 'express';
 
 import './database/connection';
 
+import routes from './routes';
+
 const app = express();
 
 app.use(json());
+
+app.use(routes);
 
 app.listen(3333, () => console.log('Server is running.'));
