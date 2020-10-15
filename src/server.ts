8a9b6154c7663addf241a280f1be48fa0ab8d982
hugo miranda <hugo.miranda.souza@gmail.com>
2020-10-14 21:57:46 -0300

    enable access to uploaded files

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express, { json } from 'express';
 
+import path from 'path';
+
 import './database/connection';
 
 import routes from './routes';
@@ -9,5 +11,6 @@ const app = express();
 app.use(json());
 
 app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
 app.listen(3333, () => console.log('Server is running.'));
